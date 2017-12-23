e9d2e76f31cf612a3ac9e275952e2df145940942
Ed <taehwa917@gmail.com>
2017-12-24 06:28:22 +0900

    taehwa_login

--- donation/forms.py
+++ donation/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Customers
+from django.contrib.auth.models import User
 
 class LoginForm(forms.ModelForm):
     class Meta:
-        model = Customers
-        fields = ['customer_mail', 'customer_pw']
+        # model = Customers
+        # fields = ['customer_mail', 'customer_pw']
+        model = User
+        fields = ['username', 'password']
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
--- donation/views.py
+++ donation/views.py
@@ -1,10 +1,15 @@
 from .models import *
 from django import forms
+from .forms import *
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.views import generic, View
 from django.shortcuts import redirect
 from django.views.generic.edit import CreateView, FormView
-
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -24,16 +29,46 @@ class UserCreate(CreateView):
     model = Customers
     fields = ['customer_name, customer_pw, customer_mail']
 
-class LoginView(View):
-    template_name = 'donation/login.html'
+# class LoginView(View):
+#     template_name = 'donation/login.html'
+#
+#     def post(self,request):
+#         form = LoginForm(request.POST)
+#         customer_mail = request.POST['customer_mail']
+#         customer_pw = request.POST['customer_pw']
+#         user = Customers.object.filter(customer_pw=customer_pw, customer_mail=customer_mail)
+#
+#         if user.exists():
+#             return redirect('index')
+#         else:
+#             return HttpResponse('로그인 실패. 다시 시도해보세요')
 
-    def post(self,request):
+def signin(request):
+    if request.method == "POST":
         form = LoginForm(request.POST)
-        customer_mail = request.POST['customer_mail']
-        customer_pw = request.POST['customer_pw']
-        user = Customers.object.filter(customer_pw=customer_pw, customer_mail=customer_mail)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            # return render(request, 'donation/UsQuakesindex.html')
+            return redirect('donation:index')
 
-        if user.exists():
-            return redirect('index')
         else:
-            return HttpResponse('로그인 실패. 다시 시도해보세요')
+            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+    else:
+        form = LoginForm()
+        return render(request, 'donation/login.html', {'form': form})
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            # return render(request, 'donation/UsQuakesindex.html')
+            return redirect('donation:index')
+    else:
+        form = UserForm()
+        return render(request, 'donation/signup.html', {'form': form})
