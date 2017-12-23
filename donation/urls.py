4b3e87d3d1ce05ea98e8dd92c9fbed4e80ec111b
MinJu <kmju1997@gmail.com>
2017-12-24 01:52:55 +0900

    minju modify url and views.py

--- donation/urls.py
+++ donation/urls.py
@@ -8,6 +8,6 @@ app_name = 'donation'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name = "index"),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = "detail"),
-    url(r'^login/$', views.UserCreate.as_view(), name="login"),
-    #no signup url
+    url(r'^signup$', views.UserCreate.as_view(), name="signup"),
+    #no login url
 ]
